5552cd5784d7303f59ed796b383f1474a036ec28
Emilia Vandamme <emilia.audrey@gmail.com>
2020-08-11 13:21:20 +0100

    up to 5

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+end
